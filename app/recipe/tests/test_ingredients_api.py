afbe760bea71666984f1780af89c5c295de87ea2
shishira <knshishira@gmail.com>
2022-01-17 08:11:10 +0530

    Added custom recipe detailed view.

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -75,7 +75,7 @@ class PrivateIngredientsAPITests(TestCase):
             name=payload['name']
         ).exists()
         self.assertTrue(exists)
-        
+         
     def test_create_ingredient_invalid(self):
         """Test creating invalid ingredient fails"""
         payload = {'name': ''}
